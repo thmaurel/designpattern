d13879e594188f1497a798b098f7b71d0673a37a
Thibaud Maurel <maurel.thibaud@free.fr>
2020-12-17 12:24:35 +0100

    singleton + consignes jungle

---
+++ jungle.rb
@@ -0,0 +1,18 @@
+# On veut créer un modèle Jungle
+# On veut créer un modèle Lion
+
+# Lorsqu'on crée la jungle, on veut décider du nombre de lions dedans
+# et qu'ils soient créés automatiquement.
+
+# ex: Jungle.new(5)
+# => crée une jungle et 5 lions (et stocker les 5 lions ds la jungle)
+
+# L'objectif est que notre modèle Jungle puisse créer des jungles ac tous types
+# d'animaux
+
+
+# ???
+# => une jungle ac 5 crocodiles
+
+# ???
+# => une jungle ac 5 lions
--- log.rb
+++ log.rb
@@ -1,9 +1,25 @@
-class Log
+# require 'singleton'
 
+class Log
+  # include Singleton
+  @@instance = Log.new
+
+  def self.instance
+    @@instance
+  end
+
+  private_class_method :new
 end
 
-log1 = Log.new
-log2 = Log.new
+# log1 = Log.new
+# log2 = Log.new
 
-p log1
+# p log1
+# p log2
+
+log = Log.instance
+p log
+
+log2 = Log.instance
 p log2
+
