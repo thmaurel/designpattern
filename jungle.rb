c5462055bd47e572c20a12c5417064f42da0db5d
Thibaud Maurel <maurel.thibaud@free.fr>
2020-12-17 15:39:35 +0100

    add plant to jungle

--- jungle.rb
+++ jungle.rb
@@ -9,14 +9,20 @@
 # Ajouter des plantes à la jungle pour pouvoir créer des lions ac des arbres
 # ou des crocodiles ac des algues sans rajouter de méthode aux factories concrètes.
 
+# permettre à la classe Jungle de créer toutes les combinaisons d'animaux/plantes possibles
+
 # Ajouter un contrôle pour être sûr de créer des lions ac des arbres (resp croco/algues)
 # et qu'on ne puisse pas créer des lions ac des algues.
 
 class Jungle
-  def initialize(nb_animals)
+  def initialize(nb_animals, nb_plants)
     @animals = []
     nb_animals.times do |i|
-      @animals << new_animal("Animal #{i + 1}")
+      @animals << new_organism(:animal ,"Animal #{i + 1}")
+    end
+    @plants = []
+    nb_plants.times do |i|
+      @plants << new_organism(:plant ,"Plant #{i + 1}")
     end
   end
 
@@ -25,20 +31,47 @@ class Jungle
       animal.eat("food")
       animal.speak
     end
+    @plants.each do |plant|
+      plant.grow
+    end
+  end
+
+  def new_organism
+    # crée les instances d'animaux et de plantes que l'utilisateur souhaite créer.
   end
 end
 
-class LionsJungle < Jungle
-  def new_animal(name)
-    Lion.new(name)
+class LionsTreesJungle < Jungle
+  def new_organism(type, name)
+    return Lion.new(name) if type == :animal
+    return Tree.new(name) if type == :plant
   end
 end
 
-class CrocodilesJungle < Jungle
-  def new_animal(name)
-    Crocodile.new(name)
+class CrocodilesAlgaesJungle < Jungle
+  def new_organism(type, name)
+    return Crocodile.new(name) if type == :animal
+    return Algae.new(name) if type == :plant
   end
 end
+
+
+class Plant
+  def initialize(name)
+    @name = name
+  end
+
+  def grow
+    puts "#{@name} is growing !"
+  end
+end
+
+class Tree < Plant
+end
+
+class Algae < Plant
+end
+
 
 # On veut créer un modèle Lion
 class Lion
@@ -69,8 +102,8 @@ class Crocodile
   end
 end
 
-jungle = CrocodilesJungle.new(3)
-jungle.one_day
+jungle = CrocodilesAlgaesJungle.new(3, 5)
+p jungle
 
 
 # L'objectif est que notre modèle Jungle puisse créer des jungles ac tous types
